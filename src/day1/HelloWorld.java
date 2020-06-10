4f2ffc82cadc1e36e1882ad63b18d411efb0a0a4
Urgyen Shakya <urgyenshakya@gmail.com>
2020-06-10 09:27:11 +0545

    a1

---
+++ src/day1/HelloWorld.java
@@ -0,0 +1,144 @@
+package day1;
+
+public class HelloWorld {
+	
+//method
+	//public --> acess modifiers 
+	//static --> 
+	//void --> return type
+	//main --> name of the Method
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//print line
+		
+		//Variables?
+		//VAry + able;
+		
+		//store value, and something that changes;
+		
+	//	<datatype>
+		
+		//naming Conventions?
+		
+		//Case Sensitive
+		
+		//camel CAsing
+		
+		String nameOfMyOldScholl;
+		
+		int Age = 24;
+		
+		int age= 25;
+		
+		String age1123= "Urgyen";
+		
+		float aaple;
+		
+		double a;
+		
+		//byte;
+		//boolean;
+	//	short
+		
+		
+		
+		System.out.println("Hellllo");
+		
+		//To find out how System.out.println("How it works");
+		
+		
+		//Datatype?
+		//what type and how much
+		//size of datatype?
+		
+		//4 byte
+		int 	age1 =	10;
+		
+		//8 byte
+		double 	age2 =	10.5;
+		
+		// 1 bit
+		// 1 or 0
+		boolean isTRue = true;
+		
+		System.out.println(isTRue);
+		
+		//Primitive and Non Primitive?
+		
+	//	boolean, int, double, char, are primitive
+		
+	//	String, arrays, are non primitive
+		
+		// Operators?
+		//Arithmentic Operators
+		// %
+		
+	//	System.out.println(15%7);
+		
+		
+		//Unary Operator
+//		
+//		int a1 = 9;
+//		
+//		a1--;
+//		//Loops;
+//		a1++;
+		
+		
+//		a1++;
+//		a1 = a1 + 1;
+//		
+		//System.out.println(a1);
+		
+		//RElational Operations
+		
+		//Control Statements
+		//if, else 
+	//	> , < , > =, <= , ==  , !=
+		
+//		if(10!=10) {
+//			System.out.println("Ok");
+//		}
+//		
+		
+		//Logical Operator?
+		
+		// Logic gates for AND and OR
+		
+//		f . f  --> f
+//		||
+//		
+//		t .. f --> f
+//		&&
+		
+		//Ternary Operator?
+		// if else
+		
+//		int num1= 25;
+//		int num2 =10;
+//		
+//	//	(Condition)? if TRue  : if False
+//		
+//		//int min = num1>num2 ? num2 : num1;
+//		
+//		System.out.println(min);
+		
+		//Assignments Operator;
+		int a11=0;
+		a11= a11+1;
+		a11++;
+	//	Post Increment
+		
+		a11+=2;
+		//Assignment Operator
+		
+		
+		//Decsion Control
+		
+		
+		
+	}
+
+}
