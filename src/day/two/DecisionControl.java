074e44a3d2b918e61bdae7838c18910952cba458
Urgyen Shakya <urgyenshakya@gmail.com>
2020-06-11 09:16:09 +0545

    June 11

---
+++ src/day/two/DecisionControl.java
@@ -0,0 +1,184 @@
+package day.two;
+
+public class DecisionControl {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// What can we do in programming?
+		// 1. do something in one line;
+		
+//		int x=10;
+//		
+//		System.out.println(x);
+//		
+//		System.out.println("Urgyen");
+		
+		//2. do something again and again?
+		//Loops
+		//for, while, do while
+		
+		//3. to do something if the condition matches;
+		//DEcision Controls;
+		//if condition
+		//if else condition
+		//else if
+		
+		
+//		int age =15;
+//		
+//		if(age>=18) {
+//			System.out.println("Eligle to VOTE");
+//		}
+//		
+//		else {
+//			System.out.println("Not Eliglible TO vote");
+//		}
+		
+		
+		//Scenario
+//		age= 
+//		upto 10 --> Child
+//		11- 19 --> teen
+//		20 -35 --> Adult
+//		35+ OLD
+		
+		//else if
+//		
+//		int age=36;
+//		if(age<=10) {
+//			System.out.println("Child");
+//		}
+//		else if(age<20) {
+//			System.out.println("TEeen");
+//		}
+//		else if( age<36) {
+//			System.out.println("Adult");
+//		}
+//		else {
+//			System.out.println("OLD Enough");
+//		}
+		
+		//Scenario;
+		//Electricity;
+		//units less than 100 = per unit is 10
+		//unit less than 300 == per unit is 15
+		//unit less than 500 == per unit 20
+		//more than 500 == per unit 25;
+		
+//		int unit=2000;
+//		double amount;
+//		
+//		if(unit<100) {
+//			amount= unit*10;
+//			System.out.println(amount);
+//		}
+//		else if(unit< 300) {
+//			amount = unit * 15;
+//			System.out.println(amount);
+//		}
+//		else if( unit<500) {
+//			amount = unit * 20;
+//			System.out.println(amount);
+//		}
+//		else {
+//			amount = unit * 25;
+//			System.out.println(amount);
+//		}
+//		
+		
+		//Scenario
+//		weekDay =7;
+//		Print SAturday
+//		
+//		weekDay =4
+//		print wednesday
+		
+		//DEcision SWITCH;
+		
+//		int weekDay =10;
+//		
+//		switch(weekDay) {
+//		
+//		case 1:
+//			System.out.println("Sunday");
+//			break;
+//		
+//		case 2:
+//			System.out.println("Monday");
+//			break;
+//		
+//		case 3:
+//			System.out.println("Tuesday");
+//			break;
+//				
+//		case 4:
+//			System.out.println("Wednesday");	
+//			break;
+//					
+//		case 5:
+//			System.out.println("Thursday");
+//			 break;
+//			
+//		case 6:
+//			System.out.println("Friday");
+//			break;
+//			
+//		case 7:
+//			System.out.println("Saturday");
+//			break;
+//			
+//		default:
+//			System.out.println("Just input 1 to 7");
+//		
+//		}
+//		
+//		String name="Apple";
+//		
+//		System.out.println(name.length());
+//		
+//		
+//		//System is built in class? from which package??
+//		//java.lang;
+//		
+//		System.out.print("World");
+//		
+//Loops???	
+		
+		//for
+		
+		//while
+		
+		//do while
+
+		//index initialization
+		int i=0;
+//		
+//		//condition check
+//		while(i<10) {
+//			System.out.println(i + " : Hello World");
+//			//increment
+//			i++;
+//		}
+		
+//		do {
+//			System.out.println(i+" : Hello World ");
+//			
+//			i++;
+//		}while(i<=10);
+		
+//		
+//		for(int j=0; j<100;j++) {
+//			
+//			System.out.println("JAVA is Easy");
+//		
+//		}
+		
+		// Diwas;; list of name of students;
+		// for loop
+		
+		
+		
+	}
+
+}
